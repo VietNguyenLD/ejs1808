04df6737c15c80decc600b9392b3d67752d62317
VietNguyenLD <59373260+VietNguyenLD@users.noreply.github.com>
2020-01-02 13:17:14 +0700

    make it better

--- index.js
+++ index.js
@@ -9,10 +9,11 @@ const arrPeo = [
     {name: 'Viet', age: 19},
     {name: 'Long', age: 20}
 ];
+const arrSubject = ['MongoDB', 'Angola', 'Python'];
 app.locals.arrPeople =arrPeo;
 
 app.get('/', (req, res) => res.render('home'));
-app.get('/learn', (req, res) => res.render('learn', { username :'Viet Quoc'}));
+app.get('/learn', (req, res) => res.render('learn', { username :'Viet Quoc', arrSubject : arrSubject}));
 
 reload(app);
 const server = require('http').createServer(app);
