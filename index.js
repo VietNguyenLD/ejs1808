c247bfea5b4f92a8b14a20acddfea39d8fac3141
VietNguyenLD <59373260+VietNguyenLD@users.noreply.github.com>
2020-01-02 12:09:13 +0700

    pass

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const reload = require('reload');
+const app = express();
+
+app.set('views', './view');
+app.set('view engine', 'ejs');
+
+const arrPeo = [
+    {name: 'Viet', age: 19},
+    {name: 'Long', age: 20}
+];
+app.locals.arrPeople =arrPeo;
+
+app.get('/', (req, res) => res.render('home'));
+app.get('/learn', (req, res) => res.render('learn', { username :'Viet Quoc'}));
+
+reload(app);
+const server = require('http').createServer(app);
+app.listen(3000), () => console.log('Server start');
